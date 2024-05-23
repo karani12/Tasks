76e5b4707556c7a87baf45a3185792f48a51bc20
karani12 <karanijohn018@gmail.com>
2024-05-23 15:28:45 +0300

    feat:add async for cron

--- cronJob.js
+++ cronJob.js
@@ -1,5 +1,19 @@
-// cronJob.js
-const cron = require('node-cron');
 const { processEmailQueue } = require('./services/NotificationService');
+const cron = require('node-cron');
 
-cron.schedule('* * * * *', processEmailQueue);
+const startApplication = async () => {
+  try {
+    // Schedule the email processing task to run every minute
+    cron.schedule('* * * * *', async () => {
+      console.log('Running cron job to process email queue');
+      await processEmailQueue();
+    });
+
+    console.log('Cron job scheduled successfully');
+  } catch (error) {
+    
+    console.error('Failed to start application:', error);
+  }
+};
+
+startApplication();
