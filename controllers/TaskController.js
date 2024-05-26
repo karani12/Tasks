4c9a129fd591c1841cfd27c0aff8b6a123887717
karani12 <karanijohn018@gmail.com>
2024-05-26 14:33:38 +0300

    fix:renew db seeding

--- controllers/TaskController.js
+++ controllers/TaskController.js
@@ -38,6 +38,7 @@ const getAllTasks = async (req, res) => {
 
    
     const tasks = await taskService.getAllTasks();
+
     res.status(200).json(tasks);
   } catch (error) {
     res.status(500).json({ error: error.message });
